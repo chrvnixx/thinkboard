c069c5e4c22ffa2d6d2e0002324741088b390316
chrvnixx <emmanueladekola91@gmail.com>
2026-01-08 11:11:55 +0100

    fixed typo in axios.js file

--- frontend/note-app/src/lib/axios.js
+++ frontend/note-app/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
 const api = axios.create({
-  baseUrl: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 export default api;
